27c073f008056ce33284cad5125dd84456ad918b
amyvzeng <amyzeng.am@gmail.com>
2018-09-09 21:35:41 +0200

    Using Express to serve index.html

--- captcha/app.js
+++ captcha/app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
+
 app.listen(8000, function () {
   console.log('Example app listening on port 8000!');
 });
