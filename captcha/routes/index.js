358839d9445bb709fe14bd297b4a3f7352315303
amyvzeng <amyzeng.am@gmail.com>
2018-09-25 16:41:06 +0200

    Setting up project skeleton

--- captcha/routes/index.js
+++ captcha/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var instructions = require('./instructions');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 module.exports = router;
